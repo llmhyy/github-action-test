14166ef885aa6d472c72e95f87f7dbeebf5693e9
Yun Lin <llmhyy@gmail.com>
2023-11-10 00:22:40 +0800

    new

---
+++ src/main/java/com/VulnerableApp.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class VulnerableApp {
+
+    public void searchProducts() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter product name to search:");
+        String productName = scanner.nextLine();
+
+        String query = "SELECT * FROM products WHERE name = '" + productName + "'";
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test", "user", "pass");
+             Statement statement = connection.createStatement()) {
+
+            // WARNING: This statement is vulnerable to SQL injection
+            statement.executeQuery(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new VulnerableApp().searchProducts();
+    }
+}
