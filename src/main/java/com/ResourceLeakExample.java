f1e8d9c99bb929d40e05f512ed87d2f629ec213b
Yun Lin <llmhyy@gmail.com>
2023-11-10 04:57:43 +0800

    af

--- src/main/java/com/ResourceLeakExample.java
+++ src/main/java/com/ResourceLeakExample.java
@@ -35,7 +35,7 @@ public class ResourceLeakExample {
 	
 	public static void main(String[] args) {
 		String password;
-        password = "12345"; 
+        password = "12345";  
         System.out.println(password);
     }
 
