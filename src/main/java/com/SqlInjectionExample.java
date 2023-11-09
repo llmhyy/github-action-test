3a1a7498f9f0cf657389af700450efed68a38a98
Yun Lin <llmhyy@gmail.com>
2023-11-10 01:05:01 +0800

    test2

--- src/main/java/com/SqlInjectionExample.java
+++ src/main/java/com/SqlInjectionExample.java
@@ -9,56 +9,17 @@ import java.sql.Statement;
 
 public class SqlInjectionExample {
 
-//    public void doLogin(String user, String pass) {
-//        Connection conn = null;
-//        Statement stmt = null;
-//        try {
-//            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=myuser&password=mypass");
-//
-//            // Unsafe SQL query constructed by concatenating strings.
-//            String query = "SELECT * FROM users WHERE user = '" + user + "' AND password = '" + pass + "'";
-//
-//            stmt = conn.createStatement();
-//            stmt.executeQuery(query);
-//
-//            // ...
-//
-//        } catch (SQLException se) {
-//            // Handle errors for JDBC
-//            se.printStackTrace();
-//        } finally {
-//            // Finally block used to close resources
-//            try {
-//                if (stmt != null) {
-//                    stmt.close();
-//                }
-//            } catch (SQLException se2) {
-//            }
-//            try {
-//                if (conn != null) {
-//                    conn.close();
-//                }
-//            } catch (SQLException se) {
-//                se.printStackTrace();
-//            }
-//        }
-//    }
-    
     public void doLogin(String user, String pass) {
         Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
+        Statement stmt = null;
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=myuser&password=mypass");
 
-            // Safe SQL query using prepared statements.
-            String query = "SELECT * FROM users WHERE user = ? AND password = ?";
+            // Unsafe SQL query constructed by concatenating strings.
+            String query = "SELECT * FROM users WHERE user = '" + user + "' AND password = '" + pass + "'";
 
-            pstmt = conn.prepareStatement(query);
-            pstmt.setString(1, user);
-            pstmt.setString(2, pass);
-
-            rs = pstmt.executeQuery();
+            stmt = conn.createStatement();
+            stmt.executeQuery(query);
 
             // ...
 
@@ -68,12 +29,12 @@ public class SqlInjectionExample {
         } finally {
             // Finally block used to close resources
             try {
-                if (rs != null) {
-                    rs.close();
+                if (stmt != null) {
+                    stmt.close();
                 }
-                if (pstmt != null) {
-                    pstmt.close();
-                }
+            } catch (SQLException se2) {
+            }
+            try {
                 if (conn != null) {
                     conn.close();
                 }
@@ -82,4 +43,43 @@ public class SqlInjectionExample {
             }
         }
     }
+    
+//    public void doLogin(String user, String pass) {
+//        Connection conn = null;
+//        PreparedStatement pstmt = null;
+//        ResultSet rs = null;
+//        try {
+//            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=myuser&password=mypass");
+//
+//            // Safe SQL query using prepared statements.
+//            String query = "SELECT * FROM users WHERE user = ? AND password = ?";
+//
+//            pstmt = conn.prepareStatement(query);
+//            pstmt.setString(1, user);
+//            pstmt.setString(2, pass);
+//
+//            rs = pstmt.executeQuery();
+//
+//            // ...
+//
+//        } catch (SQLException se) {
+//            // Handle errors for JDBC
+//            se.printStackTrace();
+//        } finally {
+//            // Finally block used to close resources
+//            try {
+//                if (rs != null) {
+//                    rs.close();
+//                }
+//                if (pstmt != null) {
+//                    pstmt.close();
+//                }
+//                if (conn != null) {
+//                    conn.close();
+//                }
+//            } catch (SQLException se) {
+//                se.printStackTrace();
+//            }
+//        }
+//    }
 }
