a54da865d92347576d3a2d23ec4db5983b4b9a2b
Yun Lin <llmhyy@gmail.com>
2023-11-09 23:52:22 +0800

    a

--- src/test/java/com/BugExample.java
+++ src/test/java/com/BugExample.java
@@ -4,18 +4,12 @@ public class BugExample {
 
 	private String name;
 
-	public BugExample(String name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 
 	public String getName() {
 		return name.toUpperCase();
-	}
-
-	public static void main(String[] args) {
-		BugExample example = new BugExample(null);
-		String name = example.getName();
-		System.out.println(name);
 	}
 
 }
